369bf1e1a980eae0c0aa33985c5142fb4b930923
Saad Aslam <saadusufzai@gmail.com>
2020-11-26 18:12:32 +0500

    Update diariesSlice.ts

--- src/features/diary/diariesSlice.ts
+++ src/features/diary/diariesSlice.ts
@@ -11,12 +11,12 @@ const diaries = createSlice({
       });
       state.push(...diariesToSave);
     },
-    updateDiary(state, {payload }: PayloadAction<Diary>){
-        const {id} = payload;
-        const diaryIndex = state.findIndex((diary) => diary.id === id)
-        if(diaryIndex !== -1){
-            state.splice(diaryIndex, 1, payload);
-        }
-    }
+    updateDiary(state, { payload }: PayloadAction<Diary>) {
+      const { id } = payload;
+      const diaryIndex = state.findIndex((diary) => diary.id === id);
+      if (diaryIndex !== -1) {
+        state.splice(diaryIndex, 1, payload);
+      }
+    },
   },
 });
